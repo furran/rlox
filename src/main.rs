1036d9134fd6377dd32620bfb742ec63f6963bab
Francisco Machado <franciscomontenegrolm@gmail.com>
2026-03-22 16:11:01 -0300

    add entry point

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,72 @@
-use rlox::{common::Value, object::NativeFn, vm::vm::repl};
-use rlox_gc::Gc;
+use std::io::Write;
+
+use rlox::vm::{VM, vm::VMError};
+
+pub fn repl() {
+    let stdin = std::io::stdin();
+    let mut vm = VM::new(std::io::stdout());
+
+    loop {
+        print!(">>> ");
+        vm.output.flush().unwrap();
+
+        let mut buffer = String::new();
+        stdin.read_line(&mut buffer).unwrap();
+
+        while !is_complete(&buffer) {
+            print!("... ");
+            vm.output.flush().unwrap();
+            stdin.read_line(&mut buffer).unwrap();
+        }
+
+        if let Err(e) = vm.interpret(&buffer) {
+            match e {
+                VMError::CompileError(errors) => {
+                    for error in errors {
+                        eprintln!("[CompileError] {}", error);
+                    }
+                }
+                VMError::RuntimeError(msg) => eprintln!("[RuntimeError] {}", msg),
+            }
+        }
+    }
+}
+
+fn is_complete(source: &str) -> bool {
+    let mut depth = 0;
+    let mut in_string = false;
+
+    for c in source.chars() {
+        match c {
+            '"' => in_string = !in_string,
+            '{' if !in_string => depth += 1,
+            '}' if !in_string => depth -= 1,
+            _ => {}
+        }
+    }
+    depth == 0
+}
+
+pub fn run_file(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let source = std::fs::read_to_string(path)?;
+    let output = std::io::stdout();
+    let mut vm = VM::new(output);
+    vm.interpret(&source)?;
+    Ok(())
+}
 
 fn main() {
-    println!("Value size: {}", std::mem::size_of::<Value>());
-    dbg!(std::mem::size_of::<NativeFn>()); // 8 — fn pointer
-    dbg!(std::mem::size_of::<&'static str>()); // 16 — fat pointer
-    dbg!(std::mem::size_of::<Gc<()>>()); // 8 — thin pointer
-    dbg!(std::mem::size_of::<f64>());
-    let _ = repl();
+    let args = std::env::args().collect::<Vec<String>>();
+    match args.len() {
+        1 => repl(),
+        2 => {
+            if let Err(e) = run_file(&args[1]) {
+                eprintln!("{}", e);
+            }
+        }
+        _ => {
+            eprintln!("Usage: rlox [file]");
+            std::process::exit(64)
+        }
+    }
 }
--- src/vm/vm.rs
+++ src/vm/vm.rs
@@ -183,7 +183,7 @@ pub struct VM<W: Write> {
     frames: Vec<CallFrame>,
     open_upvalues: HashMap<usize, Gc<Upvalue>>,
     init_string: Gc<ObjString>,
-    output: W,
+    pub output: W,
 }
 
 impl<W: Write> VM<W> {
@@ -203,14 +203,6 @@ impl<W: Write> VM<W> {
 
         vm.define_native("clock", 0, native_clock);
         vm
-    }
-
-    pub fn run_file(path: &str) -> Result<(), Box<dyn std::error::Error>> {
-        let source = std::fs::read_to_string(path)?;
-        let output = std::io::stdout();
-        let mut vm = VM::new(output);
-        vm.interpret(&source)?;
-        Ok(())
     }
 
     pub fn interpret(&mut self, source: &str) -> VMResult {
@@ -772,48 +764,3 @@ struct Roots<'a>(
     &'a GlobalIndices,
     &'a Gc<ObjString>,
 );
-
-pub fn repl() -> std::io::Result<()> {
-    let stdin = std::io::stdin();
-    let mut vm = VM::new(std::io::stdout());
-
-    loop {
-        print!(">>> ");
-        vm.output.flush().unwrap();
-
-        let mut buffer = String::new();
-        stdin.read_line(&mut buffer).unwrap();
-
-        while !is_complete(&buffer) {
-            print!("... ");
-            vm.output.flush().unwrap();
-            stdin.read_line(&mut buffer).unwrap();
-        }
-
-        if let Err(e) = vm.interpret(&buffer) {
-            match e {
-                VMError::CompileError(errors) => {
-                    for error in errors {
-                        eprintln!("[CompileError] {}", error);
-                    }
-                }
-                VMError::RuntimeError(msg) => eprintln!("[RuntimeError] {}", msg),
-            }
-        }
-    }
-}
-
-fn is_complete(source: &str) -> bool {
-    let mut depth = 0;
-    let mut in_string = false;
-
-    for c in source.chars() {
-        match c {
-            '"' => in_string = !in_string,
-            '{' if !in_string => depth += 1,
-            '}' if !in_string => depth -= 1,
-            _ => {}
-        }
-    }
-    depth == 0
-}
